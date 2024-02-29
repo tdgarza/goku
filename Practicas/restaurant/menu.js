81952995bbc3215664f1d9a5fb7a5da43f985131
Tomas Garza Moya <tomasgarzamoya@Tomass-MacBook-Air-2.local>
2024-02-29 15:11:21 -0600

    restaurante

---
+++ Practicas/restaurant/menu.js
@@ -0,0 +1,25 @@
+fetch('menu.json')
+.then(response => response.json())
+.then(data => {
+    const menuContainer = document.getElementById('menu-container');
+    data.items.forEach(category => {
+        const categoryTitle = document.createElement('h2');
+        categoryTitle.textContent = category.category;
+        menuContainer.appendChild(categoryTitle);
+        
+        const table = document.createElement('table');
+        const tableHead = `<tr><th>Foto</th><th>Descripción</th><th>Precio</th></tr>`;
+        let tableBody = '';
+        
+        category.items.forEach(item => {
+            tableBody += `<tr>
+                            <td><img src="${item.image}" alt="${item.name}"></td>
+                            <td>${item.name} - ${item.description}</td>
+                            <td>${item.price}</td>
+                          </tr>`;
+        });
+        
+        table.innerHTML = tableHead + tableBody;
+        menuContainer.appendChild(table);
+    });
+});
